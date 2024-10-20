8fdb9ecc08fddfce5ec1b3acbf54a3d191e00ea1
Rodrigues Santos, Carolina <Carolina.Rodrigues-Santos@adesso.de>
2024-10-20 12:19:41 +0200

    added first components to show days of the week and assign workout groups to them

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {WorkoutPlannerComponent} from './workout-planner/workout-planner.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, WorkoutPlannerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
