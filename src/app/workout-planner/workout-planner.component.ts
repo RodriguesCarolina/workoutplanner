e013a5706922b8133ac268200ba61352840b2333
Rodrigues Santos, Carolina <Carolina.Rodrigues-Santos@adesso.de>
2024-10-20 17:56:08 +0200

    added calendar functionality to the current month

--- src/app/workout-planner/workout-planner.component.ts
+++ src/app/workout-planner/workout-planner.component.ts
@@ -12,16 +12,38 @@ import {WorkoutGroupComponent} from '../workout-group/workout-group.component';
 })
 
 export class WorkoutPlannerComponent {
+   monthNames = [
+  "January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+  currentMonth = new Date().getMonth();
+  currentMonthName = this.monthNames[this.currentMonth];
+  daysInMonth: {date: number, day: number}[] = [];
   weekDays: string[] = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-  selectedDay: string  | null = null; // Tracks the selected day to add a group.
-  workoutGroupsByDay: {
-    [key: string]: string[]
-  } = {}
+  selectedDay: number | null = null;
+  // Tracks the selected day to add a group.
+  workoutGroupsByDay: { [key: string]: string[] } = {}
 
   constructor() {
   }
 
-  addWorkoutGroup(day: string) {
+  ngOnInit(): void {
+    this.generateDaysForMonth(new Date().getFullYear(), new Date().getMonth());
+    console.log(this.currentMonthName);
+  }
+
+  generateDaysForMonth(year: number, month: number): void {
+    const firstDayOfMonth = new Date(year, month, 1).getDay();
+    const daysInThisMonth = new Date(year, month + 1, 0).getDate();
+
+    this.daysInMonth = [];
+
+    for (let day = 1; day <= daysInThisMonth; day++) {
+      this.daysInMonth.push({ date: day, day: (firstDayOfMonth + day - 1) % 7 });
+    }
+  }
+
+  addWorkoutGroup(day: number) {
     this.selectedDay = day;
     // logic: open a list of workout group components. From there, we can plan the workout of this day.
     console.log(`Add workout group to ${day}`);
